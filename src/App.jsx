969bd031632e8ee84dae34c1988835e5306e0ef3
Nam Tang <nam.tang@live.fr>
2022-11-28 10:24:56 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className="">
+      <h1 className='bg-emerald-200 font-bold text-3xl uppercase text-center tracking-[10px] py-4'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
