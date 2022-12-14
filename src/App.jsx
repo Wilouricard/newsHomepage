1998cee569151d103169e820e650dc5427851ba9
Nam Tang <nam.tang@live.fr>
2022-12-14 13:33:59 +0100

    add dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,26 @@ import Navbar from "./components/Navbar";
 import News from "./components/News";
 
 function App() {
+  const [open, setOpen] = React.useState(true);
   return (
+    <>
+      <div className={`absolute h-screen w-full flex ${open?'hidden':'inactive'}`}>
+        <div className="bg-Dark-grayish-blue w-1/3 opacity-60"></div>
+        <div className="bg-Off-white w-2/3 h-screen flex flex-col items-end">
+          <button className="p-8" onClick={()=>{setOpen(!open)}}><img src="public\images\icon-menu-close.svg" alt="close button" /></button>
+          <div className="w-full flex flex-col mt-auto mb-auto px-8 text-2xl gap-8">
+            <a href="#">Home</a>
+            <a href="#">New</a>
+            <a href="#">Popular</a>
+            <a href="#">Trending</a>
+            <a href="#">Categories</a>
+          </div>
+        </div>
+      </div>
     <main className="mx-5">
       <nav className="flex justify-between py-8">
         <img src="\images\logo.svg" alt="logo" className="w-12" />
-        <button className="">
+        <button className="" onClick={()=>{setOpen(!open)}}>
           <img src="\images\icon-menu.svg" alt="" />
         </button>
       </nav>
@@ -64,31 +79,46 @@ function App() {
       </div>
       <div className="py-12 flex flex-col gap-6">
         <div className="flex items-center">
-          <img src="\images\image-retro-pcs.jpg" alt="retro pc image" className="w-1/3"/>
+          <img
+            src="\images\image-retro-pcs.jpg"
+            alt="retro pc image"
+            className="w-1/3"
+          />
           <div className="pl-6 flex flex-col gap-3">
-          <h1 className=" text-Grayish-blue font-extrabold text-5xl">01</h1>
-          <h2 className="font-bold text-xl">Reviving Retro PCs</h2>
-          <p className="">What happens when old PCs are given modern upgrades?</p>
+            <h1 className=" text-Grayish-blue font-extrabold text-5xl">01</h1>
+            <h2 className="font-bold text-xl">Reviving Retro PCs</h2>
+            <p className="">
+              What happens when old PCs are given modern upgrades?
+            </p>
           </div>
         </div>
         <div className="flex items-center">
-          <img src="\images\image-top-laptops.jpg" alt="retro pc image" className="w-1/3" />
+          <img
+            src="\images\image-top-laptops.jpg"
+            alt="retro pc image"
+            className="w-1/3"
+          />
           <div className="pl-6 flex flex-col gap-3">
-          <h1 className=" text-Grayish-blue font-extrabold text-5xl">02</h1>
-          <h2 className="font-bold text-xl">Top 10 Laptops of 2022</h2>
-          <p>Our best picks for various needs and budgets.</p>
+            <h1 className=" text-Grayish-blue font-extrabold text-5xl">02</h1>
+            <h2 className="font-bold text-xl">Top 10 Laptops of 2022</h2>
+            <p>Our best picks for various needs and budgets.</p>
           </div>
         </div>
         <div className="flex items-center">
-          <img src="\images\image-gaming-growth.jpg" alt="retro pc image" className="w-1/3" />
+          <img
+            src="\images\image-gaming-growth.jpg"
+            alt="retro pc image"
+            className="w-1/3"
+          />
           <div className="pl-6 flex flex-col gap-3">
-          <h1 className=" text-Grayish-blue font-extrabold text-5xl">03</h1>
-          <h2 className="font-bold text-xl">The Growth of Gaming</h2>
-          <p>How the pandemic has sparked fresh opportunities.</p>
+            <h1 className=" text-Grayish-blue font-extrabold text-5xl">03</h1>
+            <h2 className="font-bold text-xl">The Growth of Gaming</h2>
+            <p>How the pandemic has sparked fresh opportunities.</p>
           </div>
         </div>
       </div>
     </main>
+    </>
   );
 }
 
